bb126c58a035c49b57e284d1156a71b11ed7ea1e
Junior Cercado Vasquez <juniorcercado@peu.edu.pe>
2021-01-07 18:58:56 -0500

    top nav nuevo

--- resources/views/layouts/topnav.blade.php
+++ resources/views/layouts/topnav.blade.php
@@ -1,8 +1,43 @@
 <nav class="navbar navbar-top navbar-expand navbar-dark bg-primary border-bottom">
     <div class="container-fluid">
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        
-        <form class="navbar-search navbar-search-light form-inline mr-sm-3" id="navbar-search-main">
+
+        <div class="navbar-nav align-items-center  mr-md-auto ">
+          <!-- ================================= ICONO AGREGAR FILE ================================= -->
+          <li class="nav-item">
+            <a class="nav-link" href="" type="button" aria-haspopup="true" aria-expanded="false">
+
+              <i class="fas fa-file-signature" style="font-size: 28px;"></i>
+
+            </a>
+          </li>
+          <!-- ================================= ICONO AGREGAR FILE ================================= -->
+          <li class="nav-item">
+            <a class="nav-link" href="" type="button" aria-haspopup="true" aria-expanded="false">
+
+              <i class="fas fa-file-signature" style="font-size: 28px;"></i>
+
+            </a>
+          </li>
+          <!-- ================================= ICONO AGREGAR FILE ================================= -->
+          <li class="nav-item">
+            <a class="nav-link" href="" type="button" aria-haspopup="true" aria-expanded="false">
+
+              <i class="fas fa-file-signature" style="font-size: 28px;"></i>
+
+            </a>
+          </li>
+          <!-- ================================= ICONO AGREGAR FILE ================================= -->
+          <li class="nav-item">
+            <a class="nav-link" href="" type="button" aria-haspopup="true" aria-expanded="false">
+
+              <i class="fas fa-file-signature" style="font-size: 28px;"></i>
+
+            </a>
+          </li>
+        </div>
+        <!-- ================================= ICONO BUSCAR ================================= -->
+        <!-- <form class="navbar-search navbar-search-light form-inline mr-sm-3" id="navbar-search-main">
           <div class="form-group mb-0">
             <div class="input-group input-group-alternative input-group-merge">
               <div class="input-group-prepend">
@@ -14,11 +49,11 @@
           <button type="button" class="close" data-action="search-close" data-target="#navbar-search-main" aria-label="Close">
             <span aria-hidden="true">×</span>
           </button>
-        </form>
-        
+        </form> -->
+
         <ul class="navbar-nav align-items-center  ml-md-auto ">
           <li class="nav-item d-xl-none">
-            
+
             <div class="pr-3 sidenav-toggler sidenav-toggler-dark" data-action="sidenav-pin" data-target="#sidenav-main">
               <div class="sidenav-toggler-inner">
                 <i class="sidenav-toggler-line"></i>
@@ -27,26 +62,30 @@
               </div>
             </div>
           </li>
+
+
+
           <li class="nav-item d-sm-none">
             <a class="nav-link" href="#" data-action="search-show" data-target="#navbar-search-main">
               <i class="ni ni-zoom-split-in"></i>
             </a>
           </li>
+
           <!--<li class="nav-item dropdown">
             <a class="nav-link" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
               <i class="ni ni-bell-55"></i>
             </a>
             <div class="dropdown-menu dropdown-menu-xl  dropdown-menu-right  py-0 overflow-hidden">
-              
+
               <div class="px-3 py-3">
                 <h6 class="text-sm text-muted m-0">You have <strong class="text-primary">13</strong> notifications.</h6>
               </div>
-              
+
               <div class="list-group list-group-flush">
                 <a href="#!" class="list-group-item list-group-item-action">
                   <div class="row align-items-center">
                     <div class="col-auto">
-                      
+
                       <img alt="Image placeholder" src="/argon/assets/img/theme/team-1.jpg" class="avatar rounded-circle">
                     </div>
                     <div class="col ml--2">
@@ -65,7 +104,7 @@
                 <a href="#!" class="list-group-item list-group-item-action">
                   <div class="row align-items-center">
                     <div class="col-auto">
-                      
+
                       <img alt="Image placeholder" src="/argon/assets/img/theme/team-2.jpg" class="avatar rounded-circle">
                     </div>
                     <div class="col ml--2">
@@ -84,7 +123,7 @@
                 <a href="#!" class="list-group-item list-group-item-action">
                   <div class="row align-items-center">
                     <div class="col-auto">
-                      
+
                       <img alt="Image placeholder" src="/argon/assets/img/theme/team-3.jpg" class="avatar rounded-circle">
                     </div>
                     <div class="col ml--2">
@@ -103,7 +142,7 @@
                 <a href="#!" class="list-group-item list-group-item-action">
                   <div class="row align-items-center">
                     <div class="col-auto">
-                      
+
                       <img alt="Image placeholder" src="/argon/assets/img/theme/team-4.jpg" class="avatar rounded-circle">
                     </div>
                     <div class="col ml--2">
@@ -122,7 +161,7 @@
                 <a href="#!" class="list-group-item list-group-item-action">
                   <div class="row align-items-center">
                     <div class="col-auto">
-                      
+
                       <img alt="Image placeholder" src="/argon/assets/img/theme/team-5.jpg" class="avatar rounded-circle">
                     </div>
                     <div class="col ml--2">
@@ -139,13 +178,13 @@
                   </div>
                 </a>
               </div>
-              
+
               <a href="#!" class="dropdown-item text-center text-primary font-weight-bold py-3">View all</a>
             </div>
           </li>-->
           <li class="nav-item dropdown">
             <a class="nav-link" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              <i class="ni ni-ungroup"></i>
+              <i class="ni ni-ungroup" style="font-size: 28px;"></i>
             </a>
             <div class="dropdown-menu dropdown-menu-lg dropdown-menu-dark bg-default  dropdown-menu-right ">
               <div class="row shortcuts px-4">
@@ -188,12 +227,30 @@
               </div>
             </div>
           </li>
+
+          <!-- ================================= ICONO EMAIL ================================= -->
           <li class="nav-item">
             <a class="nav-link" href="" type="button" aria-haspopup="true" aria-expanded="false">
-              <i class="ni ni-email-83"></i>
+              <i class="ni ni-email-83" style="font-size: 28px;"></i>
+
+            </a>
+          </li>
+
+          <!-- ================================= ICONO AYUDA ================================= -->
+          <li class="nav-item">
+            <a class="nav-link" href="" type="button" aria-haspopup="true" aria-expanded="false">
+              <i class="far fa-question-circle " style="font-size: 28px;"></i>
+            </a>
+          </li>
+
+          <!-- ================================= ICONO EXCLAMACION ================================= -->
+          <li class="nav-item">
+            <a class="nav-link" href="" type="button" aria-haspopup="true" aria-expanded="false">
+              <i class="fas fa-info-circle" style="font-size: 28px;"></i>
             </a>
           </li>
         </ul>
+        <!-- ================================= PERFIL DEL USUARIO LOGUADO ============================= -->
         <ul class="navbar-nav align-items-center  ml-auto ml-md-0 ">
           <li class="nav-item dropdown">
             <a class="nav-link pr-0" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
